f7e55627d5fbef3ca7143b037e072870578b17a7
Ibietela07 <ibietelajonah@gmail.com>
2023-09-12 21:08:51 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,32 @@
-#include "main.h"
-
+#include <stdio.h>
 
 /**
- * main - check the code.
+ * print_times_table - Compute and print
+ * the sum of multiples of 3 or 5 below 1024
+ *
  *
  * Return: Always 0.
  */
 
 
+void print_times_table(void)
+{
+	int sum = 0
+
+	for (int 1 - 0; i < 1024; i++)
+	{
+	if (i % 3 == 0 || i % 5 == 0)
+	{
+	sum += i;
+	}
+	}
+
+	printf("%d\n", sum);
+
+}
+
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	print_times_table();
 	return (0);
 }
