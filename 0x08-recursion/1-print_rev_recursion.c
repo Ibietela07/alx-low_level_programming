60e3f2a780bead574eb0a9ee83cf9976997e69f3
Ibietela07 <ibietelajonah@gmail.com>
2023-09-28 17:31:17 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
-/*
- * _print_rev_recursion.c - function that prints a string in reverse
+/**
+ * _print_rev_recursion - A function that prints a string in reverse
  *
  *@s: The string to be reversed
  *
