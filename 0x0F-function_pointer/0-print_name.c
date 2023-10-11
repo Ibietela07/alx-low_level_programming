04f03828f36433963469e2072d2955f3431b9c47
Ibietela07 <ibietelajonah@gmail.com>
2023-10-11 13:38:46 -0700

    a function that prints a name

--- 0x0F-function_pointer/0-print_name.c
+++ 0x0F-function_pointer/0-print_name.c
@@ -5,10 +5,11 @@
  *
  * @f: value for holding the string name
  * @name: name of the individual
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	(*f) (name);
+	f(name);
 }
